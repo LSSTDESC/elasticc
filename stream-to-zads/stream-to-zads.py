3663c0aaccd4e62461a75a18eca6c518d821865e
Rob Knop (Nersc) <raknop@lbl.gov>
2022-08-10 08:25:21 -0700

    Add logging in the database when an elasticc alert is streamed to ZADS (not tested yet).

--- stream-to-zads/stream-to-zads.py
+++ stream-to-zads/stream-to-zads.py
@@ -3,6 +3,8 @@ import os
 import re
 import io
 import time
+import requests
+import json
 import logging
 import logging.handlers
 import datetime
@@ -26,7 +28,9 @@ class AlertStreamer:
     def __init__( self, alertdirs=None, schemafile=None, kafka_broker='brahms.lbl.gov:9092',
                   kafka_topic='elasticc-test-only-1', compression_factor=10,
                   campaign_start=datetime.datetime(2022,7,6,7,0,0), nights_done_cache="/nightcache/nightsdone.lis",
-                  simnight0=60274, simnight1=61378, dry_run=False, logger=_logger ):
+                  simnight0=60274, simnight1=61378,
+                  tom_url='https://desc-tom.lbl.gov', tom_username='rknop', tom_passwdfile='/secrets/tom_passwd',
+                  dry_run=False, logger=_logger ):
         self.logger = logger
         
         if alertdirs is None:
@@ -73,8 +77,72 @@ class AlertStreamer:
         else:
             self.logger.warning( f"No nights done cache {self.nights_done_cache}." )
             self.nights_done = []
-        
 
+        self.tom_url = tom_url
+        self.tom_username = tom_username
+        self.tom_passwdfile = tom_passwdfile
+        with open( tom_passwdfile ) as ifp:
+            self.tom_passwd = ifp.readline().strip()
+
+    def log_into_tom( self ):
+        rqs = requests.session()
+        rqs.get( f'{self.tom_url}/accounts/login/' )
+        res = rqs.post( f'{self.tom_url}/accounts/login/',
+                        data={ "username": self.tom_username,
+                               "password": self.tom_passwd,
+                               "csrfmiddlewaretoken": rqs.cookies['csrftoken'] } )
+        if res.status_code != 200:
+            raise RuntimeError( f"Failed to log in; http status: {res.status_code}" )
+        if 'Please enter a correct' in res.text:
+            raise RuntimeError( "Failed to log in.  I think.  Put in a debug break and look at res.text" )
+        rqs.headers.update( { 'X-CSRFToken': rqs.cookies['csrftoken'] } )
+        return rqs
+
+    def notify_tom( self, rqs, ids ):
+        if len(ids) == 0:
+            return
+        
+        outercountdown = 5
+        while outercountdown >= 0:
+            countdown = 5
+            try:
+                while countdown >= 0:
+                    try:
+                        res = rqs.post( f'{self.tom_url}/elasticc/markalertsent', json=ids )
+                        if res.status_code != 200:
+                            self.logger.error( f"Got status_code={res.status_code} from TOM" )
+                            raise RuntimeError( "Got status_code={res.status_code} from TOM" )
+                        data = json.loads( res.text )
+                        if ( not 'status' in data ) or ( data['status'] != 'ok' ):
+                            strio = io.StringIO( "" )
+                            if data['status'] == 'error':
+                                strio.write( "Error return from TOM:\n" )
+                                strio.write( f"   message: {data['message']}\n" )
+                                strio.write( f"   exception: {data['exception']}\n" )
+                                strio.write( f"   traceback:\n{data['traceback']}\n" )
+                            else:
+                                strio.write( f"Unexpected return from TOM: {data}" )
+                            self.logger.error( strio.getvalue() )
+                            raise RuntimeError( strio.getvalue() )
+                        countdown = -1
+                    except Exception as e:
+                        if countdown >= 0:
+                            self.logger.error( "Failed too many times, bailing." )
+                            raise e
+                        self.logger.error( "Retrying in 1 second..." )
+                        time.sleep( 1 )
+                        countdown -= 1
+                outercountdown = -1
+            except Exception as e:
+                if outercountdown <= 0:
+                    self.logger.error( "Outer tom notification failed too many times, bailing." )
+                    raise e
+                time.sleep( 1 )
+                self.logger.error( "Going to try logging back into the tom." )
+                rqs = self.log_into_tom()
+                outercountdown -= 1
+        return rqs
+    
     def stream_todays_batch( self, alert_delay=0, diffmjd_delay=0.2, diffnight_delay=5 ):
         now = datetime.datetime.now( datetime.timezone.utc )
         curday = ( now - self.t0 ).days
@@ -102,6 +170,8 @@ class AlertStreamer:
                                                    'batch.size': self.kafka_batch_size_bytes,
                                                    'linger.ms': self.kafka_linger_ms
                                                   } )
+            rqs = self.log_into_tom()
+            
         nstreamed = 0
         bytesstreamed = 0
 
@@ -151,6 +221,7 @@ class AlertStreamer:
             self.logger.info( f"Streaming {len(alertfilenames)} alerts for night {n}" )
 
             lastmjd = ''
+            idsproduced = []
             for alertfile in alertfilenames:
                 match = nameparse.search( alertfile )
                 if not match:
@@ -161,6 +232,8 @@ class AlertStreamer:
                     if diffmjd_delay > 0:
                         if not self.dry_run:
                             producer.flush()
+                            rqs = self.notify_tom( rqs, idsproduced )
+                            idsproduced = []
                         self.logger.debug( f'Starting exposure mjd {mjd}; '
                                            f'have {"fake-" if self.dry_run else " "}streamed '
                                            f'{nightnstreamed} for night {n}; '
@@ -175,12 +248,15 @@ class AlertStreamer:
                     fastavro.write.schemaless_writer( alertbytes, self.schema, alert )
                     if not self.dry_run:
                         producer.produce( self.kafka_topic, alertbytes.getvalue() )
+                        idsproduced.append( alert['alertId'] )
                     nightbytesstreamed += len( alertbytes.getvalue() )
                     nightnstreamed += 1
                     if alert_delay > 0:
                         time.sleep( alert_delay )
 
             producer.flush()
+            rqs = self.notify_tom( rqs, idsproduced )
+            idsproduced = []
             self.logger.info( f'{"Fake-s" if self.dry_run else "S"}treamed {nightnstreamed} total alerts for night {n} '
                               f'({nightbytesstreamed/1024/1024:.3f} MiB).' )
             nstreamed += nightnstreamed
@@ -192,6 +268,8 @@ class AlertStreamer:
 
         # This next line is gratuitous
         producer.flush()
+        rqs = self.notify_tom( rqs, idsproduced )
+        idsproduced = []
         self.logger.info( f"Done with today's batch.  {'Fake-s' if self.dry_run else 'S'}treamed {nstreamed} alerts "
                           f"({bytesstreamed/1024/1024:.3f} MiB)." )
 
